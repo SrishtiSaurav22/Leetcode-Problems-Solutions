03f9bad85292682ac3d610602405f25c36a216e2
Srishti Saurav <103064640+SrishtiSaurav22@users.noreply.github.com>
2023-02-25 22:59:16 +0530

    medium

--- reverse_integer.cpp
+++ reverse_integer.cpp
@@ -1,64 +1,36 @@
-/*
- Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
-
- Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
-
- EXAMPLE 1:
- Input: x = 123
- Output: 321
-
- EXAMPLE 2:
- Input: x = -123
- Output: -321
-
- EXAMPLE 3:
- Input: x = 120
- Output: 21
-
-Constraints:
--231 <= x <= 231 - 1
-*/
-
-#include<bits/stdc++.h>
-#include<typeinfo>
-
-using namespace std;
-
-/*
- INT_MIN:
- -2^31:
- -2147483648
-
- INT_MAX:
- (2^31)-1:
- 2147483647
-*/
-
-int reverseInteger(int num)
-{
-    int nnum,d,sum=0;
+int reverse(int num) 
+    {
+    int nnum;
+    int d,sum=0;
     int c=0;
 
-    if(num<0) nnum=num*-1;
+    if(num>INT_MAX || num<INT_MIN)
+        return 0;
+
+    if(num==INT_MIN) return 0; 
+
+    // this should be 'else if' but the previous 'if' returns 0, so it's fine
+    if(num<0) nnum=num*(-1);
     else nnum=num;
 
     while(nnum)
     {
         if((++c)==10) // the sum is 9 digits long now, about to be made 10 digits in this 10th iteration
         {
-            int diff=INT_MAX/10-sum;
+            int diff=(INT_MAX/10)-sum;
 
             if(diff<0)
                 return 0;
 
             else
             {
-                if(nnum%10>7)
+                if((nnum%10)>7)
                     return 0;
 
                 else
                 {
-                    sum=sum*10+d;
+                    d=nnum%10;
+                    sum=(sum*10)+d;
                     break;
                 }
             }
@@ -69,18 +41,7 @@ int reverseInteger(int num)
         nnum=nnum/10;
     }
 
-    if(num<0) return sum*-1;
+    if(num<0) return sum*(-1);
     else return sum;
-}
-
-int main()
-{
-    int num=-2147483412;
-    // you can only input values that lie in the range: [INT_MIN, INT_MAX]
-
-    cout<<"Original number: "<<num<<endl;
-    cout<<"Reversed number: "<<reverseInteger(num)<<endl;
-
-    cout<<endl;
-    return 0;
-}
+        
+    }
