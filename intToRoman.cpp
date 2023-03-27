a7f0b50df9a8fd950c20ed37d040acad7b9b9a03
Srishti Saurav <103064640+SrishtiSaurav22@users.noreply.github.com>
2023-03-27 23:58:34 +0530

    medium, some test cases not passed

---
+++ intToRoman.cpp
@@ -0,0 +1,148 @@
+int i,d,q;
+
+    string oneTo9(int num)
+    {
+        string strcur="";
+
+        if(num>=1 && num<=3)
+        {
+            for(i=0;i<num;i++)
+            strcur=strcur+"I";
+        }
+
+        else if(num==4)
+        strcur="IV";
+
+        else if(num>=5 && num<=8)
+        {
+            strcur="V";
+
+            for(i=0;i<num-5;i++)
+            strcur=strcur+"I";
+        }
+
+        else if(num==9)
+        strcur="IX";
+
+        return strcur;
+    }
+
+    string tenTo99(int num)
+    {
+        string strcur="";
+
+        d=num%10;
+        q=num/10;
+
+        // finding remDigit
+        string remDigit=oneTo9(d);
+        
+        // finding mainDigit
+        if(q>=1 && q<=3)
+        {
+            for(i=0;i<q;i++)
+            strcur=strcur+"X";
+        }
+
+        else if(q==4)
+        {
+            strcur="XL";
+        }
+
+        else if(q>=5 && q<=8)
+        {
+            strcur="L";
+
+            for(i=0;i<q-5;i++)
+            strcur=strcur+"X";
+        }
+
+        else if(q==9)
+        {
+            strcur="XC";
+        }
+
+        string mainDigit=strcur;
+
+        return mainDigit+remDigit;
+    }
+
+    string hundredTo999(int num)
+    {
+        string strcur="";
+        
+        d=num%100;
+        q=num/100;
+
+        // finding remDigit
+        string remDigit=tenTo99(d);
+
+        // finding mainDigit
+        if(q>=1 && q<=3)
+        {
+            for(i=0;i<q;i++)
+            strcur=strcur+"C";
+        }
+
+        else if(q==4)
+        {
+            strcur="CD";
+        }
+
+        else if(q>=5 && q<=8)
+        {
+            strcur="D";
+
+            for(i=0;i<q-5;i++)
+            {
+                strcur=strcur+"C";
+            }
+        }
+
+        else if(q==9)
+        {
+            strcur="CM";
+        }
+
+        string mainDigit=strcur;
+
+        return mainDigit+remDigit;
+    }
+
+    string thousandTo3999(int num)
+    {
+        string strcur="";
+
+        d=num%1000;
+        q=num/1000;
+
+        // finding remDigit
+        string remDigit=hundredTo999(d);
+
+        // finding mainDigit
+        for(i=0;i<q;i++)
+        {
+            strcur=strcur+"M";
+        }
+
+        string mainDigit=strcur;
+
+        return mainDigit+remDigit;
+    }
+
+    string intToRoman(int num) 
+    {
+        if(num<10)
+        return oneTo9(num);
+
+        else if(num<100)
+        return tenTo99(num);
+
+        else if(num<1000)
+        return hundredTo999(num);
+
+        else
+        return thousandTo3999(num);
+
+        return "";
+    }
