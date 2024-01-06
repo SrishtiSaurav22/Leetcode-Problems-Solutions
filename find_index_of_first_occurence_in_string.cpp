9374e1246c7a7985b4bad7a794447878ff8b4963
Srishti Saurav <103064640+SrishtiSaurav22@users.noreply.github.com>
2024-01-06 16:42:02 +0530

    Updated find_index_of_first_occurence_in_string.cpp
    Could not solve the question. I have 3 variations of the same function but there are different issues arising with all three. Check the comments.

--- find_index_of_first_occurence_in_string.cpp
+++ find_index_of_first_occurence_in_string.cpp
@@ -1,4 +1,12 @@
-int strStr(string haystack, string needle) 
+/*
+ Here, the first function did not pass some test cases. The second did pass the known test cases (don't know about edge cases), but exceeded the time limit. The third passed the known test cases in CodeBlocks but not in the leetcode compiler (again, don't know about the edge cases).
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int strStr1(string haystack, string needle) 
     {
         int i=0, j=0;
 
@@ -29,3 +37,70 @@ int strStr(string haystack, string needle)
         }
         return -1;
     }
+
+int strStr2(string haystack, string needle)
+{
+        int i=0, j=0, matching=-1;
+
+        int sizeHaystack=haystack.size();
+        int sizeNeedle=needle.size();
+
+        while(i<sizeHaystack && j<sizeNeedle)
+        {
+            if(haystack[i]==needle[j])
+            {
+                if(matching==-1)
+                {
+                    matching=i;
+                }
+
+                ++i;
+                ++j;
+            }
+
+            else
+            {
+                j=0;
+                matching=-1;
+            }
+        }
+
+        return matching;
+}
+
+int strStr3(string haystack, string needle)
+    {
+        int i;
+
+        while(i<haystack.size()-needle.size()+1)
+        {
+            string temp=haystack.substr(i,needle.size());
+            cout<<"\ntemp="<<temp<<endl;
+            //Using substr(<beginning_position>, <size>);
+
+            if(needle.compare(temp)==0)
+            {
+                return i;
+            }
+
+            else
+            {
+                ++i;
+            }
+        }
+
+        return -1;
+    }
+
+int main()
+{
+    string haystack="sadbutsad", needle="sad";
+
+    cout<<strStr2(haystack, needle)<<endl;
+    cout<<strStr3(haystack, needle)<<endl;
+
+    cout<<endl;
+    return 0;
+}
+
+
